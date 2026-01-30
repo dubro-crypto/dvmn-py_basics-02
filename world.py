a9536265b30488eb93b6fbedb2c913a31fe5974a
dubro-crypto <dubrovin.misha9977@gmail.com>
2026-01-30 19:51:17 +0300

    Add files via upload
    2

--- world.py
+++ world.py
@@ -1,6 +1,6 @@
 from transliterate import translit
-print(translit("Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...", 'uk'))
 from num2words import num2words
+print(translit("Ladies and gentlemen, I'm 78 years old and I finally got 15 minutes of fame once in a lifetime and I guess that this is mine. People have also told me to make these next few minutes escruciatingly embarrassing and to take vengeance of my enemies. Neither will happen.More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic Storage for last 40. When I was 8...", 'uk'))
 print(78, end="-")
 print(num2words(78,lang='uk'))  
 print(15,end="-")
