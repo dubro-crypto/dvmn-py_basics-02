54ca81284b123f55b9c2bbc14eb41546b4e525bb
dubro-crypto <dubrovin.misha9977@gmail.com>
2026-04-22 18:14:04 +0300

    Add files via upload

--- uyi.py
+++ uyi.py
@@ -1,13 +1,17 @@
 from PIL import Image
+
 image = Image.open("lenna.jpg")
 rgb_image = image.convert("RGB")
 red, green, blue = rgb_image.split()
 shift = 50
-red_cropped = red.crop((shift, 0, red.width, red.height))
-blue_cropped = blue.crop((0, 0, red.width - shift, red.height))
-green_cropped = green.crop((shift / 2, 0, red.width - shift / 2, red.height))
-final_image = Image.merge("RGB", (red_cropped, green_cropped, blue_cropped))
+red_left = red.crop((shift, 0, red.width, red.height))
+red_middle = red.crop((shift / 2, 0, red.width - shift / 2, red.height))
+red_final = Image.blend(red_left, red_middle, 0.5)
+blue_right = blue.crop((0, 0, blue.width - shift, blue.height))
+blue_middle = blue.crop((shift / 2, 0, blue.width - shift / 2, blue.height))
+blue_final = Image.blend(blue_right, blue_middle, 0.5)
+green_final = green.crop((shift / 2, 0, green.width - shift / 2, green.height))
+final_image = Image.merge("RGB", (red_final, green_final, blue_final))
 final_image.save("final_glitch.jpg")
-max_size = (80, 80)
 final_image.thumbnail((80, 80))
 final_image.save("avatar.jpg")
